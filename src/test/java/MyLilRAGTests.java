906f1c84db1aaa1f8ef8555a5e690c06ff746d51
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-09-10 23:30:01 -0400

    I think it works now

--- src/test/java/MyLilRAGTests.java
+++ src/test/java/MyLilRAGTests.java
@@ -5,27 +5,16 @@ import junit.framework.TestSuite;
 public class MyLilRAGTests 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
     public MyLilRAGTests( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( MyLilRAGTests.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
     public void testFormatAnswer()
     {
 	String result = MyLilRAG.formatAnswer("the elements of a set in a particular order. For example, if we have a set of three elements {a, b, c}, the permutations of this set are:");
